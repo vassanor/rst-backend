8e2e01da11d5330eb50abb35a2f5cece7c5f9b4d
noradrenal1ne <vassanor@gmail.com>
2023-04-09 15:30:37 +0300

    create project

--- roseltorg-backend/src/main/java/com/norch/roseltorgbackend/controllers/SupplierController.java
+++ roseltorg-backend/src/main/java/com/norch/roseltorgbackend/controllers/SupplierController.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Optional;
 
 //@CrossOrigin(origins = "http://localhost:3000") //open for specific port
-@CrossOrigin() // open for all ports
 @RestController
 public class SupplierController {
 
@@ -42,73 +41,6 @@ public class SupplierController {
 
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
-
-    @PostMapping("/supplier")
-    public ResponseEntity<Supplier> newSupplier(@RequestBody Supplier supplier) {
-        Supplier newSupplier = supplierRepository
-                .save(Supplier.builder()
-                        .inn(supplier.getInn())
-                        .procedure_qty(supplier.getProcedure_qty())
-                        .win_part(supplier.getWin_part())
-                        .registration_date(supplier.getRegistration_date())
-                        .license_activity_type(supplier.getLicense_activity_type())
-                        .avg_staff_qty(supplier.getAvg_staff_qty())
-                        .msp_type(supplier.getMsp_type())
-                        .msp_category(supplier.getMsp_category())
-                        .life_time(supplier.getLife_time())
-                        .build());
-        return new ResponseEntity<>(newSupplier, HttpStatus.OK);
-    }
-
-    @PutMapping("/supplier/{id}")
-    public ResponseEntity<Supplier> updateSupplier(@PathVariable("id") long id, @RequestBody Supplier supplier) {
-
-        //check if employee exist in database
-        Supplier newSupplier = getSupplier(id);
-        if (newSupplier != null) {
-            newSupplier.setInn((supplier.getInn()));
-            newSupplier.setLicense_activity_type(supplier.getLicense_activity_type());
-            newSupplier.setMsp_type(supplier.getMsp_type());
-            newSupplier.setRegistration_date(supplier.getRegistration_date());
-            newSupplier.setProcedure_qty(supplier.getProcedure_qty());
-            newSupplier.setMsp_category(supplier.getMsp_category());
-            newSupplier.setAvg_staff_qty(supplier.getAvg_staff_qty());
-            newSupplier.setLife_time(supplier.getLife_time());
-            newSupplier.setWin_part(supplier.getWin_part());
-            return new ResponseEntity<>(supplierRepository.save(newSupplier), HttpStatus.OK);
-        }
-
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @DeleteMapping("/supplier/{id}")
-    public ResponseEntity<HttpStatus> deleteSupplierById(@PathVariable("id") long id) {
-        try {
-            //check if employee exist in database
-            Supplier supplier = getSupplier(id);
-
-            if (supplier != null) {
-                supplierRepository.deleteById(id);
-                return new ResponseEntity<>(HttpStatus.OK);
-            }
-
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @DeleteMapping("/suppliers")
-    public ResponseEntity<HttpStatus> deleteAllCustomers() {
-        try {
-            supplierRepository.deleteAll();
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
     }
